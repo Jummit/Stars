71677f19496898ff2255319a4265d082b808ea55
Jummit <jummit@web.de>
2018-02-23 13:58:54 +0100

    updated fw, buttons work now

--- libs/fw.lua
+++ libs/fw.lua
@@ -1,28 +1,29 @@
-local fw = {
-  update = function(elements, dt)
-    for _, element in pairs(elements) do
-      if love.mouse.isDown(1) then
-        element.clicked = boxClicked(element, var2, var3)
-      else
-        if element.clicked and element.clickedFunc then element:clickedFunc(var1, var2, var3) end
-        element.clicked = false
-      end
-      if element.update then
-        element:update(event, dt)
-      end
-    end
-  end,
-  draw = function(elements)
-    for _, element in pairs(elements) do
-      element:draw()
-    end
-  end
-}
-
+local fw = {}
 function fw.boxClicked(box)
   local mouseX, mouseY = love.mouse.getPosition()
   if box.x and box.y and box.w and box.h then
     return box.x<=mouseX and box.y<=mouseY and box.x+box.w>mouseX and box.y+box.h>mouseY
+  end
+end
+function fw.getCenter(box, text)
+
+end
+function fw.update(elements, dt)
+  for _, element in pairs(elements) do
+    if love.mouse.isDown("l") then
+      element.clicked = fw.boxClicked(element, var2, var3)
+      if element.clicked and element.clickedFunc then element:clickedFunc(var1, var2, var3) end
+    else
+      element.clicked = false
+    end
+    if element.update then
+      element:update(event, dt)
+    end
+  end
+end
+function fw.draw(elements)
+  for _, element in pairs(elements) do
+    element:draw()
   end
 end
 
@@ -31,18 +32,21 @@ fw.elements.button = {
   clicked = false,
   draw = function(self)
     if self.clicked then
-      love.graphics.setBackgroundColor(self.clickedCol.r, self.clickedCol.g, self.clickedCol.b)
+      love.graphics.setColor(self.clickedCol.r, self.clickedCol.g, self.clickedCol.b)
+      love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
     else
-      love.graphics.setBackgroundColor(self.color.r, self.color.g, self.color.b)
+      love.graphics.setColor(self.color.r, self.color.g, self.color.b)
+      love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
     end
-    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-    love.graphics.setBackgroundColor(self.textColor.r, self.textColor.g, self.textColor.b)
-    love.graphics.print(self.label, (self.x+self.w/2)-#self.label/2, self.y+self.h/2)
+    love.graphics.setColor(self.textColor.r, self.textColor.g, self.textColor.b)
+    local textX = (self.x+self.w/2)-love.graphics.getFont():getWidth(self.label)/2
+    local textY = self.y+self.h/2-love.graphics.getFont():getHeight(self.label)/2
+    love.graphics.print(self.label, textX, textY)
   end
 }
 
 function fw.new(type, args)
-  return setmetatable(args,{__index=elements[type]})
+  return setmetatable(args,{__index=fw.elements[type]})
 end
 
 return fw
